89b17858cab4ba991f35df08f06f9f8463d5b134
choco <superbeaver.180809@gmail.com>
2022-05-03 17:48:01 +0900

    [update] devise_for コメントを追記

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # ユーザ登録機能（registrations）、ログイン・ログアウト機能（sessions）へアクセスがあった場合
   # 下記のxxx.rbファイルを参照するように設定
+  # ※ users、controllersモデルを参照する場合は、下記のファイルを参照する
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
