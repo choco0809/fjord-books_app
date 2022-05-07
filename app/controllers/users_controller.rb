c0952a6a8c945b48b7ed75b1e0fbf7aa1f7fde16
choco <superbeaver.180809@gmail.com>
2022-05-07 14:49:01 +0900

    [update] user一覧、user詳細画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  layout 'users.html.erb'
   # GET /users
   def index
     @users = User.order(:id).page(params[:page])
@@ -8,8 +8,6 @@ class UsersController < ApplicationController
   # GET /users/1
   def show
     @user = User.find(params[:id])
-    # application.html.erbを使用しない
-    # render :layout => nil
   end
   
 end
