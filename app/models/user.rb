914d628406f48def58dc54cd8c11006da5b6e367
choco <superbeaver.180809@gmail.com>
2022-05-08 09:01:36 +0900

    [update] rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
-  validates :postal_code, format: { with: /[0-9]{7}/, message: 'は数字を入力してください'}
+  validates :postal_code, format: { with: /[0-9]{7}/, message: 'は数字を入力してください' }
   validates :postal_code, length: { is: 7 }
-  #validates :postal_code, format: { with: /\A[a-zA-Z]+\z/}
 end
