4da7b9dbfbaffa7d4b64a97a30f4820a72eb8d52
choco <superbeaver.180809@gmail.com>
2022-05-15 23:25:10 +0900

    [update] params[:id]をparams[:user_id]に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,12 @@ class UsersController < ApplicationController
   end
 
   def follows
-    user = User.find(params[:id])
+    user = User.find(params[:user_id])
     @users = user.followings.page(params[:page]).per(3).reverse_order
   end
   
   def followers
-    user = User.find(params[:id])
+    user = User.find(params[:user_id])
     @users = user.followers.page(params[:page]).per(3).reverse_order
   end
 end
