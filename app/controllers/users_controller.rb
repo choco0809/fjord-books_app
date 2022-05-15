566896c49e64aa450466ef6c68d4414cbd1126af
choco <superbeaver.180809@gmail.com>
2022-05-15 23:18:54 +0900

    [update] has_manyで定義したモデル名を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @following_users = @user.following_user
-    @follower_users = @user.follower_user
+    @following_users = @user.followings
+    @follower_users = @user.followers
   end
 
   def follows
-    user = User.find(params[:user_id])
-    @users = user.following_user.page(params[:page]).per(3).reverse_order
+    user = User.find(params[:id])
+    @users = user.followings.page(params[:page]).per(3).reverse_order
   end
   
   def followers
-    user = User.find(params[:user_id])
-    @users = user.follower_user.page(params[:page]).per(3).reverse_order
+    user = User.find(params[:id])
+    @users = user.followers.page(params[:page]).per(3).reverse_order
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :avatar
-  has_many :follower, class_name: "FollowsRelationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "FollowsRelationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following_user, through: :follower, source: :followed
-  has_many :follower_user, through: :followed, source: :follower
+  has_one_attached :avatar  
+  # フォロー
+  has_many :following_user, class_name: "FollowsRelationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :following_user, source: :followed
+  # フォロワー
+  has_many :follower_user, class_name: "FollowsRelationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :follower_user, source: :follower
 
   def follow(user_id)
-    follower.create(followed_id: user_id)
+    following_user.create(followed_id: user_id)
   end
 
   def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy
+    following_user.find_by(followed_id: user_id).destroy
   end
 
   def following?(user)
-    following_user.include?(user)
+    followings.include?(user)
   end
 end
