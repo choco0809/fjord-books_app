e0c2bcaefda44315ca609d8d4b0d815047101f8f
choco <superbeaver.180809@gmail.com>
2022-05-16 00:04:48 +0900

    [update] follower_id + followed_idをユニーク制約に設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_14_114151) do
+ActiveRecord::Schema.define(version: 2022_05_15_145938) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_05_14_114151) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_follows_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_follows_relationships_on_follower_id"
   end
 
