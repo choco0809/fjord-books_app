9071da0c411b7d2f5dcb91ab3b78df05485607ff
choco <superbeaver.180809@gmail.com>
2022-05-22 16:46:05 +0900

    [update] request.refererを用いて遷移元をリダイレクトするのではなく、明示的にリダイレクトするように変更

--- app/controllers/followsrelationships_controller.rb
+++ app/controllers/followsrelationships_controller.rb
@@ -3,11 +3,11 @@
 class FollowsrelationshipsController < ApplicationController
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    redirect_to user_path(params[:user_id])
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    redirect_to user_path(params[:user_id])
   end
 end
