b3021e3b69a28b3e44d7c62d5b7b1b8ce5b7ef4f
choco <superbeaver.180809@gmail.com>
2022-05-22 20:39:32 +0900

    [update] フォロー/フォロワー一覧を表示機能を追加

--- app/controllers/users/follows_controller.rb
+++ app/controllers/users/follows_controller.rb
@@ -1,2 +1,6 @@
 class Users::FollowsController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    @users = user.followings.page(params[:page]).per(3).reverse_order
+  end
 end
