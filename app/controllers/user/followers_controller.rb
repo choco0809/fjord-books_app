274cfa2f4287be8a81221ac6dc2cd4e4f5637223
choco <superbeaver.180809@gmail.com>
2022-05-22 20:07:58 +0900

    [update] Follows、FollowersコントローラーをUsers配下に再作成

--- app/controllers/user/followers_controller.rb
+++
@@ -1,2 +0,0 @@
-class User::FollowersController < ApplicationController
-end
--- test/controllers/users/follows_controller_test.rb
+++ test/controllers/users/follows_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class User::FollowsControllerTest < ActionDispatch::IntegrationTest
+class Users::FollowsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
