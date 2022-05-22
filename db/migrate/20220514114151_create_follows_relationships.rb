9fac456de9d0ce56744d5c6272e60f1d9a73bc53
choco <superbeaver.180809@gmail.com>
2022-05-22 15:51:17 +0900

    [update] follows_relationshipsテーブルのfollower_id,followed_idにNOT NULL制約を追加

--- db/migrate/20220514114151_create_follows_relationships.rb
+++ db/migrate/20220514114151_create_follows_relationships.rb
@@ -1,8 +1,8 @@
 class CreateFollowsRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :follows_relationships do |t|
-      t.references :follower, foreign_key: { to_table: :users }
-      t.references :followed, foreign_key: { to_table: :users }
+      t.references :follower, foreign_key: { to_table: :users }, :null => false
+      t.references :followed, foreign_key: { to_table: :users }, :null => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -50,12 +50,11 @@ ActiveRecord::Schema.define(version: 2022_05_15_145938) do
   end
 
   create_table "follows_relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_follows_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_follows_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_follows_relationships_on_follower_id"
   end
 
