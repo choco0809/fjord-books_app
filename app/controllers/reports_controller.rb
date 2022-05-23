5e042da71a8fb47d437e1d35bcd3a46573e8ceca
choco <superbeaver.180809@gmail.com>
2022-05-23 23:42:34 +0900

    [update] 日報削除機能を追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,6 @@
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+
   def index
     @reports = Report.order(:id).page(params[:page])
   end
@@ -10,14 +12,27 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
     @report.user_id = current_user.id
-    p @report
     respond_to do |format|
       if @report.save
-        format.html { redirect_to user_reports_path, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
+        format.html { redirect_to reports_path, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
       else
         format.html { render :new }
       end
     end
+  end
+
+  def destroy
+    @report.destroy
+    respond_to do |format|
+      format.html { redirect_to reports_path, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
   end
 
   def report_params
