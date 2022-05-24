94b9bf17736ca120f0227949be4fceb3c81c0464
choco <superbeaver.180809@gmail.com>
2022-05-24 22:26:06 +0900

    [update] NOT NULL制約をコロンを使用するよう修正

--- db/migrate/20220522213557_create_reports.rb
+++ db/migrate/20220522213557_create_reports.rb
@@ -1,8 +1,8 @@
 class CreateReports < ActiveRecord::Migration[6.1]
   def change
     create_table :reports do |t|
-      t.references :user, foreign_key: true, :null => false
-      t.string :title, :null => false
+      t.references :user, foreign_key: true, null: false
+      t.string :title, null: false
       t.string :contents
 
       t.timestamps
