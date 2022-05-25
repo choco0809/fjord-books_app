f2ebec5e82a9dd909eb1324fa9813cb08e8a4643
choco <superbeaver.180809@gmail.com>
2022-05-26 01:00:28 +0900

    [update] 本詳細画面からコメントを登録できるよう機能を追加

---
+++ app/controllers/books/comments_controller.rb
@@ -0,0 +1,13 @@
+class Books::CommentsController < CommentsController
+  before_action :set_commentable, only: %i[create]
+
+  def create
+    super
+  end
+
+  private
+  def set_commentable
+    @commentable = Book.find(params[:book_id])  
+  end  
+
+end
