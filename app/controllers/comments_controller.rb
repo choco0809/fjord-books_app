13cad97fc4c1a7dad267af54ba09a9fba88bd0d0
choco <superbeaver.180809@gmail.com>
2022-05-26 01:02:36 +0900

    [delete] 不要なコメントを削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     respond_to do |format|
       if @comment.save
-        # format.html { redirect_to report_path(@comment.commentable_id), notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
         format.html { redirect_to polymorphic_path(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
       else
         format.html { render :new }
