53bce38ccc92277f6768594b530298b69ed13c2e
choco <superbeaver.180809@gmail.com>
2022-05-26 00:45:58 +0900

    [update] 日報詳細画面からコメントを登録できるよう機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = @commentable.comments.build(comments_params)  
+    @comment.user_id = current_user.id
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to report_path(@comment.commentable_id), notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+  def comments_params  
+    params.require(:comment).permit(:comment_contents)
+  end  
+
 end
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,2 +1,13 @@
-class Reports::CommentsController < ApplicationController
+class Reports::CommentsController < CommentsController
+  before_action :set_commentable, only: %i[create]
+
+  def create
+    super
+  end
+
+  private
+  def set_commentable  
+    @commentable = Report.find(params[:report_id])  
+  end  
+
 end
