085304188c7f4eeeca1696f17acb99a0c42f5928
choco <superbeaver.180809@gmail.com>
2022-05-26 23:25:58 +0900

    [update] 空白を削除

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -7,7 +7,7 @@ class Reports::CommentsController < CommentsController
 
   private
   def set_commentable  
-    @commentable = Report.find(params[:report_id])  
+    @commentable = Report.find(params[:report_id])
   end  
 
 end
