ebbc24684ae47483f43ca1e289d643bcda342a86
choco <superbeaver.180809@gmail.com>
2022-05-26 19:55:22 +0900

    [update] user_name_or_emailをApplicationHelperに追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def user_name_or_email(user)
+    @user = User.find(user[:user_id])
+    @user.name == '' ? @user.email : @user.name
+  end
 end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,6 +1,2 @@
 module ReportsHelper
-  def user_name_or_email(report)
-    @user = User.find(report[:user_id])
-    @user.name == '' ? @user.email : @user.name
-  end
 end
