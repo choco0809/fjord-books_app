3358504cc1fad3772b363e2dea8424763507cf53
choco <superbeaver.180809@gmail.com>
2022-05-27 23:05:59 +0900

    [update] 不要なコードを削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,21 +19,14 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
     respond_to do |format|
-      if @report.save
-        format.html { redirect_to reports_path, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-      else
-        format.html { render :new }
-      end
+      format.html { redirect_to reports_path, notice: t('controllers.common.notice_create', name: Report.model_name.human) }
     end
   end
 
   def update
     respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-      else
-        format.html { render :edit }
-      end
+      @report.update(report_params)
+      format.html { redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human) }
     end
   end
 
@@ -41,7 +34,6 @@ class ReportsController < ApplicationController
     @report.destroy
     respond_to do |format|
       format.html { redirect_to reports_path, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
-      format.json { head :no_content }
     end
   end
 
