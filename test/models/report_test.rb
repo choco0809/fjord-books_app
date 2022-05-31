7d17683f1698e31ff97796588c72d594e9270244
choco <superbeaver.180809@gmail.com>
2022-05-31 19:41:21 +0900

    [update] ダブルクォートをシングルクォートに変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -8,20 +8,19 @@ class ReportTest < ActiveSupport::TestCase
     @he = users(:david)
   end
 
-  test "userとtarget_userが同じ場合" do
+  test 'userとtarget_userが同じ場合' do
     my_report = Report.create(user_id: @me.id, title: '日報1', content: '初めての日報')
     assert my_report.editable?(@me)
   end
 
-  test "userとtarget_userが異なる場合" do
+  test 'userとtarget_userが異なる場合' do
     my_report = Report.create(user_id: @me.id, title: '日報1', content: '初めての日報')
     assert_not my_report.editable?(@he)
   end
 
-  test "created_atをDate型に変換する" do
+  test 'created_atをDate型に変換する' do
     my_report = Report.create(user_id: @me.id, title: '日報1', content: '初めての日報', created_at: '2022-05-30 13:15:03.841221
 ')
     assert_equal Date, my_report.created_on.class
   end
-
 end
